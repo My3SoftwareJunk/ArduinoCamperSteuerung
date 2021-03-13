1873c551d8b8b41c6026456fcc2258cccf3bf628
Julian Glatzenberger <79282705+JulianGlatzenberger@users.noreply.github.com>
2021-03-13 17:31:07 +0100

    add RST pin

--- adafruit_graphictest.ino
+++ adafruit_graphictest.ino
@@ -23,9 +23,10 @@
 // For the Adafruit shield, these are the default.
 #define TFT_DC 9
 #define TFT_CS 10
+#define TFT_RST 8
 
 // Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
 // If using the breakout, change pins as desired
 //Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
 
